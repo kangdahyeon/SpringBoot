05b7b28d60ec268a1544867e47d69d9ea599ed0b
kangdaheyon <kangdaehyon123@gamil.com>
2021-11-08 09:58:14 +0900

    study

---
+++ BoardWeb/src/main/java/com/springbook/BoardWebApplication.java
@@ -0,0 +1,12 @@
+package com.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWebApplication.class, args);
+	}
+}
